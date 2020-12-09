1b99bb783af1b72d623886c69aa0e0049c1590e3
jusung <listelion@naver.com>
2020-12-09 10:47:49 +0900

    create todolist

---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      할일 리스트 만들기
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            message: 'hi'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
